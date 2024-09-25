e943cc55f35584ee83b9ec05fe2a91152e71f930
truds99 <dudscardoso9@gmail.com>
2024-09-25 16:29:18 -0300

    dependencies and health

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express, { json } from "express";
+import httpStatus from "http-status";
+import { Request, Response } from "express";
+import 'express-async-errors'
+import dotenv from "dotenv";
+import cors from "cors";
+
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(json());
+
+app.get('/health', (req: Request, res: Response) => {
+    res.send(`I'm ok`).status(httpStatus.OK);
+})
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is up and running on port ${port}`);
+});
